339f76108705ee5ae28b6e5fb58f52b6d825c4df
FelixTheCatBG <griddesign99@gmail.com>
2019-05-03 13:08:08 +0200

    Login first try

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
+import {NgForm} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,34 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  invalidLogin: boolean;
 
-  constructor() { }
+  constructor(
+    private router:Router,
+    private http: HttpClient
+    ) { }
 
+  login(form: NgForm) {
+      let credentials = JSON.stringify(form.value);
+      console.log(credentials);
+      this.http.post("http://localhost:1337/auth/local", credentials
+       , {
+        headers: new HttpHeaders({
+          "Content-Type": "application/json"
+        })
+      }).subscribe(response => {
+        console.log(response);
+        let token = (<any>response).jwt;       
+        localStorage.setItem("jwt", token);
+        this.invalidLogin = false;
+        this.router.navigate(["/"]);
+        
+      }, err => {
+      this.invalidLogin = true;
+      });
+    }
   ngOnInit() {
+    
   }
 
 }
