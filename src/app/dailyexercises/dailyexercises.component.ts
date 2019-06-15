cec470a5064a195310e1fcd1f056dc44f4d1bb83
FelixTheCatBG <griddesign99@gmail.com>
2019-06-15 11:45:38 +0200

    Reuse dailyex component

--- src/app/dailyexercises/dailyexercises.component.ts
+++ src/app/dailyexercises/dailyexercises.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dailyexercises',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dailyexercises.component.css']
 })
 export class DailyexercisesComponent implements OnInit {
-
+  @Input() exArray : [];
+  
   constructor() { }
 
   ngOnInit() {
